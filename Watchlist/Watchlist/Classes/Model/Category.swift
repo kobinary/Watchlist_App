c340108ee4abaf48206177b24f3c36fcc81dc398
Maria <kobinary@gmail.com>
2019-03-19 10:54:36 +0100

    Added TableVC and Models
    Added main data models.
    Added TableVC and NavigationController into Storyboard.
    Added empty TableViewController.

---
+++ Watchlist/Watchlist/Classes/Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Watchlist
+//
+//  Created by Maria Ortega on 19/03/2019.
+//  Copyright © 2019 Maria Ortega. All rights reserved.
+//
+
+import UIKit
+
+struct Category {
+    let categoryID: Int
+    let title: String
+    let tabs: Array<Information>
+}
---
+++ Watchlist/Watchlist/Classes/Model/Information.swift
@@ -0,0 +1,15 @@
+//
+//  Information.swift
+//  Watchlist
+//
+//  Created by Maria Ortega on 19/03/2019.
+//  Copyright © 2019 Maria Ortega. All rights reserved.
+//
+
+import UIKit
+
+struct Information {
+    let infoID: Int
+    let name: String
+    let modified: String
+}
