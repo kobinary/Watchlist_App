2651a8e3a804beae22c8423df6c7ff225d65fb8e
Maria <kobinary@gmail.com>
2019-03-19 10:39:58 +0100

    Added Xcode project

---
+++ Watchlist/Watchlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watchlist
+//
+//  Created by Maria Ortega on 19/03/2019.
+//  Copyright © 2019 Maria Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
